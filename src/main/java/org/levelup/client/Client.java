e66f9a8236843b41aca85f2ac9bdb4796137f976
Sam <bk.simon50@gmail.com>
2019-04-30 18:17:19 +0300

    After making a json communication

--- src/main/java/org/levelup/client/Client.java
+++ src/main/java/org/levelup/client/Client.java
@@ -1,7 +1,15 @@
 package org.levelup.client;
+import org.level.up.json.JsonService;
+import org.level.up.json.configuration.JsonConfiguration;
+import org.level.up.json.impl.JsonServiceImpl;
+import org.level.up.json.test.Cat;
+
 import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
+
+import static java.lang.Integer.parseInt;
+
 
 public class Client {
 
@@ -9,25 +17,30 @@ public class Client {
 
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket(InetAddress.getByName("localhost"), 7878);
-
         BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedReader fromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         BufferedWriter toServer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-
         Thread clientSend = new Thread(new ClientSend(toServer, consoleReader));
         Thread clientReceive = new Thread(new ClientReceive(fromServer));
         clientReceive.setDaemon(true);
         clientSend.setDaemon(true);
         clientSend.start();
         clientReceive.start();
-        while (!getExitFlag()){
+        while (!getExitFlag()) {
             try {
                 Thread.sleep(200);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-
+    }
+        public static String returnJsonFromCat(String line) {
+            JsonService service = new JsonServiceImpl();
+            String[] arr = line.split(" ");
+            Cat cat = new Cat(arr[0], parseInt(arr[1]));
+            String json = service.toJson(cat);
+            System.out.println(json);
+            return json;
     }
 
     private static void setExitFlag(boolean exitFlagLoc) {
@@ -54,7 +67,8 @@ public class Client {
                 System.out.println("Enter string: ");
                 try {
                     String inputString = consoleReader.readLine();
-                    toServer.write(inputString + '\n');
+
+                    toServer.write(returnJsonFromCat(inputString) + '\n');
                     toServer.flush();
                     if (inputString.equals("exit")) {
                         setExitFlag(true);
@@ -86,10 +100,9 @@ public class Client {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                System.out.println("From server: " + stringFromServer);
+                if (stringFromServer != null) System.out.println("From server: " + stringFromServer);
             }
         }
     }
-
 }
 
